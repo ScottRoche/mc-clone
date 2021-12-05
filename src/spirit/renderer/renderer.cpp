16e5e21ed5e3250ba30cbdb3bdd74abe462580d6
ScottRoche <scott.roche24@gmail.com>
2021-12-05 14:51:08 +0000

    spirit: Renderer now draws square with extra vertex colour attributes

--- src/spirit/renderer/renderer.cpp
+++ src/spirit/renderer/renderer.cpp
@@ -14,16 +14,20 @@ namespace Spirit
 
 	static const char* vertexSource = "#version 330 core\n"
 		"layout (location = 0) in vec3 aPos;\n"
+		"layout (location = 1) in vec3 aColor;\n"
+		"out vec3 Color;\n"
 		"void main()\n"
 		"{\n"
+		"	Color = aColor;\n"
 		"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 		"}\0";
 
 	static const char* fragmentSource = "#version 330 core\n"
+		"in vec3 Color;"
 		"out vec4 FragColor;\n"
 		"void main()\n"
 		"{\n"
-		"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"	FragColor = vec4(Color, 1.0f);\n"
 		"}\0";
 
 	static bool CreateShader(unsigned int* shaderId,
@@ -50,10 +54,22 @@ namespace Spirit
 
 	void RendererInit()
 	{
+		// float verticies[] = {
+		// 	-0.5f, -0.5f, 0.0f, // left  
+		// 	 0.5f, -0.5f, 0.0f, // right 
+		// 	 0.0f,  0.5f, 0.0f  // top   
+		// };
+
 		float verticies[] = {
-			-0.5f, -0.5f, 0.0f, // left  
-			 0.5f, -0.5f, 0.0f, // right 
-			 0.0f,  0.5f, 0.0f  // top   
+			-0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // top-left
+			-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom-left
+			 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom-right
+			 0.5f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f  // top-right
+		};
+		
+		unsigned int indicies[] = {
+			0, 1, 2,
+			3, 0, 2
 		};
 
 		// Generate vertex attrib array
@@ -65,10 +81,19 @@ namespace Spirit
 		glGenBuffers(1, &vertexBuffer);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
-
+		
 		// Vertex attribs
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (void*)0);
 		glEnableVertexAttribArray(0);
+
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (void*)(sizeof(float) * 3));
+		glEnableVertexAttribArray(1);
+
+		// Create index buffer
+		unsigned int indexBuffer;
+		glGenBuffers(2, &indexBuffer);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * 6, indicies, GL_STATIC_DRAW);
 
 		// Shaders
 		unsigned int vertexShader, fragmentShader;
@@ -112,6 +137,7 @@ namespace Spirit
 	{
 		glUseProgram(shaderProgram);
 		glBindVertexArray(vertexArray);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		// glDrawArrays(GL_TRIANGLES, 0, 3);
 	}
 }
